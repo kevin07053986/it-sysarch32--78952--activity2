05285356902073fd021a43d568af2cb5e8048166
Kevin Mahusay <78952-07053986@SERVER2.DOMAIN>
2024-03-02 19:20:50 +0800

    MAHUSAY_Activity2

--- src/Content.jsx
+++ src/Content.jsx
@@ -35,6 +35,18 @@ function Content() {
         image:"https://media1.tenor.com/m/HZ417QSs3N4AAAAd/looking-up-katara.gif",
         name:"Kiawentiio",
         email: "kiawentiio@gmail.com"
+    },
+
+    {
+        image:"https://filmfare.wwmindia.com/content/2023/nov/avatarthelastairbender41699598878.jpg",
+        name:"Gordon Cormier",
+        email: "gc@gmail.com"
+    },
+
+    {
+        image:"https://64.media.tumblr.com/67fdbe247b29d433a51c6381cb327bae/d3fb5b7922b025ee-1d/s1280x1920/959f2b6329ce8fc35633ff6a0da7ff7f9801492d.jpg",
+        name:"Elizabeth Yu",
+        email: "elizabethyu@gmail.com"
     }
   ]
   return (
